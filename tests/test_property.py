533df0f4beb363827acb1841430d72f712f4cf97
Stephan Richter <stephan.richter@gmail.com>
2004-07-10 14:36:36 +0000

    Gave module menaingful doc.

--- tests/test_property.py
+++ tests/test_property.py
@@ -11,9 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""XXX short summary goes here.
-
-XXX longer description goes here.
+"""Test caching of properties.
 
 $Id$
 """
