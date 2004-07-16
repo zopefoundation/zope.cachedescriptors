02110a98852b7de4555b3e25592352d3f566bc3d
Stephan Richter <stephan.richter@gmail.com>
2004-07-16 07:54:40 +0000

    Made sure that all module doc strings have an $Id$ tag. While doing this I discovered several packages that did not have a doc string at all.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,16 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 """Cached descriptors
 
 Cached descriptors cache their output.  They take into account
@@ -23,4 +36,5 @@ Persistent descriptors:
 
      **Note**, methods haven't been implemented yet.
 
+$Id$
 """
