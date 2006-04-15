2d6638ea57158ca34bf620111200171fd61f2612
Tres Seaver <tseaver@palladion.com>
2006-04-15 18:25:49 +0000

    'package_dir' must be a relative path.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except ImportError, e:
     from distutils.core import setup, Extension
 
 setup(name='zope.cachedescriptors',
-      version='1.0',
+      version='3.3-dev',
       url='http://svn.zope.org/zope.cachedescriptors',
       license='ZPL 2.1',
       description='Zope3 Cached Descriptors',
@@ -36,7 +36,7 @@ setup(name='zope.cachedescriptors',
                        'descriptors will change the values they return.',
       
       packages=['zope', 'zope.cachedescriptors'],
-      package_dir = {'': os.path.join(os.path.dirname(__file__), 'src')},
+      package_dir = {'': 'src'},
 
       namespace_packages=['zope',],
       tests_require = ['zope.testing'],
