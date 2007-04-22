b8d20248763416740c5397608df06fb41105de89
Christian Theune <ct@gocept.com>
2007-04-22 10:55:22 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.cachedescriptors',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.cachedescriptors',
       license='ZPL 2.1',
       description='Zope3 Cached Descriptors',
