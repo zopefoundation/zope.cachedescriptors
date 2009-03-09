83e85b2660aa2ff0eba21286c77c6b749fab5e23
Dan Korostelev <nadako@gmail.com>
2009-03-09 22:58:01 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name='zope.cachedescriptors',
-    version='3.5.0dev',
+    version='3.5.1dev',
     url='http://pypi.python.org/pypi/zope.cachedescriptors',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
