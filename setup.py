50c5d96e0f041c9c962a16cb6400c366fe9ed5b6
Tres Seaver <tseaver@palladion.com>
2012-05-18 16:38:45 +0000

    Drop support for Python 2.4 / 2.5.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
 
 setup(
     name='zope.cachedescriptors',
-    version='3.5.2dev',
+    version='4.0.0dev',
     url='http://pypi.python.org/pypi/zope.cachedescriptors',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
@@ -36,6 +36,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Zope Public License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development',
