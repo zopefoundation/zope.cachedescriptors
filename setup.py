c600bf5f4936ddfece05d140222c9acd417914ee
Albertas Agejevas <alga@pov.lt>
2013-02-13 21:24:17 +0000

    Back to development: 4.0.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
 
 setup(
     name='zope.cachedescriptors',
-    version='4.0.0',
+    version='4.0.1.dev0',
     url='http://pypi.python.org/pypi/zope.cachedescriptors',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
