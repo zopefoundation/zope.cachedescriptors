bf83c1c7b9f126f11315f0dc1a5b3aca669b210d
Michael Howitz <mh@gocept.com>
2022-08-30 08:33:36 +0200

    Isort imports.

--- setup.py
+++ setup.py
@@ -19,7 +19,10 @@
 """Setup for zope.cachedescriptors package
 """
 import os
-from setuptools import setup, find_packages
+
+from setuptools import find_packages
+from setuptools import setup
+
 
 def read(*rnames):
     with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
